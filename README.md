3b94474aaf993e1ee27e594535571f6c391a11ac
andrewkeig <andrew.keig@gmail.com>
2013-03-23 22:55:09 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,47 @@
 grunt-newrelic
 ==============
 
-grunt-newrelic will generate a newrelic.js file and add it to the route of your project; with a license key and app_name pulled from a config file. 
+grunt-newrelic will generate a newrelic.js file and add it to the route of your project;
+with a license key, app_name and logging level pulled from a config file.
+
+
+## Installation
+
+Install npm package
+
+    npm install grunt-newrelic
+
+Add this line to your project's `Gruntfile.js`:
+
+    grunt.loadNpmTasks("grunt-newrelic");
+
+
+## Usage
+
+Create a config file:
+
+{
+  "NEWRELIC": {
+    "APP_NAME": "Airasoul"
+  , "LICENSE_KEY": "12345"
+  , "LOGGING_LEVEL" : "trace"
+  }
+}
+
+````
+grunt newrelic
+>> Use default config/package.json
+
+grunt newrelic:development
+>> Use config/development.json
+
+grunt newrelic:test
+>> Use config/test.json
+
+grunt newrelic:staging
+>> Use config/staging.json
+
+grunt newrelic:production
+>> Use config/production.json
+````
+
---
+++ tasks/grunt-newrelic.js
@@ -0,0 +1,38 @@
+"use strict";
+
+module.exports = function(grunt) {
+  grunt.registerTask("newrelic", "generate a newrelic.js file", function(environment) {
+
+    if (environment == null) environment = "development";
+
+    try {
+        var config_path = "config/" + environment + ".json"
+        , config_file = JSON.parse(grunt.file.read(config_path))
+        , app_name = "app_name : ['" + config_file.NEWRELIC.APP_NAME + "']"
+        , license_key = "license_key : '" + config_file.NEWRELIC.LICENSE_KEY + "'"
+        , logging_level = "level : " + config_file.NEWRELIC.LOGGING_LEVEL + "'"
+        ;
+    }
+    catch(e) {
+        grunt.log.ok("Unable to load configuration file %s", config_path);
+    }
+
+    try {
+        var newrelic_file = grunt.file.read("node_modules/newrelic/newrelic.js")
+                .replace("app_name : ['My Application']", app_name)
+                .replace("license_key : 'license key here'", license_key)
+                .replace("trace : 'trace'", logging_level);
+    }
+    catch(e) {
+        grunt.log.ok("Unable to load newrelic.js from %s", "node_modules/newrelic/newrelic.js");
+    }
+
+    try {
+        grunt.file.write("newrelic.js", newrelic_file);
+        grunt.log.ok("newrelic.js saved to root");
+    }
+    catch(e) {
+        grunt.log.ok("Unable to write newrelic.js");
+    }
+  });
+};
